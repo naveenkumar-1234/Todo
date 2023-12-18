ed5dfae92c6fa3eb80806fd8c034a58d5a46052a
naveenkumar-1234 <iamnaveenkumar23@gmail.com>
2023-12-18 19:18:44 +0530

    deployment added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Todo/",
+  server:{
+    host:'0.0.0.0'
+  }
 })
